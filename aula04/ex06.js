488f744abf18ea0f44e31e5a59e8bb59f75a189e
Fernando Piccolotto <fernandosoares.piccolotto@gmail.com>
2020-05-20 15:45:38 -0300

    Estrutura condicional no JavaScript - aula04

---
+++ aula04/ex06.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 60) { // Condição simples
+    console.log(`Você ultrapassou o limite de velocidade permitido. MULTADO! `)
+}
+console.log(`Não esqueça de usar o cinto de segurança!`)
---
+++ aula04/ex07.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Nascido em ${pais}`)
+if (pais != 'Brasil') { // condição composta
+    console.log('Você é estrangeiro')
+}else {
+    console.log('Você é brasileiro')
+}
