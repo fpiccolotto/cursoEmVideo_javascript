6de1fdccabe06da4496649a571bc621f008993b1
Fernando Piccolotto <fernandosoares.piccolotto@gmail.com>
2020-05-21 15:51:16 -0300

    Mais duas aulas com exercícios práticos

---
+++ aula08/estrutura_while.js
@@ -0,0 +1,22 @@
+var c = 1 
+while (c <= 6) {
+    console.log('Boa tarde!!')
+    c++
+}
+
+/*
+var c = 1
+do {
+    console.log('Boa tarde!!')
+    c++
+} while (c <= 6)
+*/
+
+/*
+console.log('Boa tarde!!')
+console.log('Boa tarde!!')
+console.log('Boa tarde!!')
+console.log('Boa tarde!!')
+console.log('Boa tarde!!')
+console.log('Boa tarde!!')
+*/
